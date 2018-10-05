165f8bebf8eebea067ab10f8b02a0e415228dfd0
johanwijts <johanwijts@hotmail.com>
2018-10-05 14:11:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import collections
 import time
 import bluetooth
@@ -131,21 +131,21 @@ class Wiiboard:
     # Connect to the Wiiboard at bluetooth address <address>
     def connect(self, address):
         if address is None:
-            print "Non existant address"
+            print ("Non existant address")
             return
         self.receivesocket.connect((address, 0x13))
         self.controlsocket.connect((address, 0x11))
         if self.receivesocket and self.controlsocket:
-            print "Connected to Wiiboard at address " + address
+            print ("Connected to Wiiboard at address " + address)
             self.status = "Connected"
             self.address = address
             self.calibrate()
             useExt = ["00", COMMAND_REGISTER, "04", "A4", "00", "40", "00"]
             self.send(useExt)
             self.setReportingType()
-            print "Wiiboard connected"
+            print ("Wiiboard connected")
         else:
-            print "Could not connect to Wiiboard at address " + address
+            print ("Could not connect to Wiiboard at address " + address)
 
     def receive(self):
         #try:
@@ -166,7 +166,7 @@ class Wiiboard:
             elif intype == EXTENSION_8BYTES:
                 self.processor.mass(self.createBoardEvent(data[2:12]))
             else:
-                print "ACK to data write received"
+                print ("ACK to data write received")
 
 #         self.status = "Disconnected"
 #         self.disconnect()
@@ -188,15 +188,15 @@ class Wiiboard:
 
     # Try to discover a Wiiboard
     def discover(self):
-        print "Press the red sync button on the board now"
+        print ("Press the red sync button on the board now")
         address = None
         bluetoothdevices = bluetooth.discover_devices(duration=6, lookup_names=True)
         for bluetoothdevice in bluetoothdevices:
             if bluetoothdevice[1] == BLUETOOTH_NAME:
                 address = bluetoothdevice[0]
-                print "Found Wiiboard at address " + address
+                print ("Found Wiiboard at address " + address)
         if address is None:
-            print "No Wiiboards discovered."
+            print ("No Wiiboards discovered.")
         return address
 
     def createBoardEvent(self, bytes):
@@ -209,14 +209,14 @@ class Wiiboard:
         if state == BUTTON_DOWN_MASK:
             buttonPressed = True
             if not self.buttonDown:
-                print "Button pressed"
+                print ("Button pressed")
                 self.buttonDown = True
 
         if not buttonPressed:
             if self.lastEvent.buttonPressed:
                 buttonReleased = True
                 self.buttonDown = False
-                print "Button released"
+                print ("Button released")
 
         rawTR = (int(bytes[0].encode("hex"), 16) << 8) + int(bytes[1].encode("hex"), 16)
         rawBR = (int(bytes[2].encode("hex"), 16) << 8) + int(bytes[3].encode("hex"), 16)
@@ -306,7 +306,7 @@ def main():
 
     board = Wiiboard(processor)
     if len(sys.argv) == 1:
-        print "Discovering board..."
+        print ("Discovering board...")
         address = board.discover()
     else:
         address = sys.argv[1]
@@ -319,7 +319,7 @@ def main():
     except:
         pass
 
-    print "Trying to connect..."
+    print ("Trying to connect...")
     board.connect(address)  # The wii board must be in sync mode at this time
     board.wait(200)
     # Flash the LED so we know we can step on.
@@ -328,7 +328,7 @@ def main():
     board.setLight(True)
     board.receive()
 
-    print processor.weight
+    print (processor.weight)
 
 #     # Disconnect the balance board after exiting.
 #     subprocess.check_output(["bluez-test-device", "disconnect", address])

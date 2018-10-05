39db8d04487fd59880a72f7c1e89c248a37fa613
johanwijts <johanwijts@hotmail.com>
2018-10-05 14:43:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -63,6 +63,8 @@ class EventProcessor:
             
             sys.stdout.write("\r%s" % "{}-{}-{}-{}".format(*self._eventsT))
             stdout.flush()
+            time.sleep(millis / 1000.0)
+            print("Me sleepy")
 #             sleep(1)
 
     @property

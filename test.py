026a97612fbca6de77adc845fc6026253dcff6fe
johanwijts <johanwijts@hotmail.com>
2018-10-11 12:18:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -55,15 +55,16 @@ class EventProcessor:
             self._eventsTR.append(event.topRight)
             self._eventsBL.append(event.bottomLeft)
             self._eventsBR.append(event.bottomRight)
-#             print(event.topLeft, end=" ")
+            
+            print(event.topLeft, end=" ")
 #             time.sleep(millis / 1000.0)
             
-#             w = tk.Label(root, text="Hello Tkinter!")
-            w = tk.Label(root, text=str(event.topLeft))
-#             w.config(text=str(event.topLeft))
-            root.mainloop()
+# #             w = tk.Label(root, text="Hello Tkinter!")
+#             w = tk.Label(root, text=str(event.topLeft))
+# #             w.config(text=str(event.topLeft))
+#             root.mainloop()
             sleep(5)
-            root.destroy
+#             root.destroy
     
 #             stdout.write("\r%d" % event.topLeft)
 #             stdout.write("\r%d" % event.topRight)
